8a61da8f2ba64b7609d5247ccc378fe0d5ca89e6
studybuffalo <studybuffalo@gmail.com>
2018-08-25 22:09:37 -0600

    Adding files for Read The Docs documentation

---
+++ docs/_build/html/searchindex.js
@@ -0,0 +1,1 @@
+Search.setIndex({docnames:["index"],envversion:53,filenames:["index.rst"],objects:{},objnames:{},objtypes:{},terms:{build:0,doc:0,index:0,initi:0,modul:0,packag:0,page:0,search:0,thi:0},titles:["Welcome to django-oscar-helcim\u2019s documentation!"],titleterms:{django:0,document:0,helcim:0,indic:0,oscar:0,tabl:0,test:0,welcom:0}})
