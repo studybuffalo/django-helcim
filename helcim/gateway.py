4bbd955be95d79449e1e1e5c2acde269d21b0780
studybuffalo <studybuffalo@gmail.com>
2018-09-04 13:54:02 -0600

    Adding functionality to automatically determine payment type

--- helcim/gateway.py
+++ helcim/gateway.py
@@ -28,12 +28,99 @@ def post(url, post_data={}):
     # Return the response
     return xmltodict.parse(response)
 
-def purchase():
+def determine_payment_details(details):
+    """Identifies which payment details have been provided.
+
+    Cycles through the provided details to determine the most
+    appropriate payment method. If multiple methods provided, only the
+    first match is returned (token > customer code > CC number >
+    encrypted magnetic strip > magnetic strip).
+
+    Args:
+        details (dict): A dictionary of payment details (typically
+            provided by **kwargs).
+    Returns:
+        str: the payment type detected
+    Raises:
+        SomeError: No valid payment details provided.
+    """
+
+    if 'card_token' in details and 'customer_code' in details:
+        return 'token'
+    elif 'customer_code' in details:
+        return 'customer'
+    elif 'cc_number' in details and 'cc_expiry' in details:
+        return 'cc'
+    elif 'cc_mag_encrypted' in details and 'serial_number' in details:
+        return 'mage'
+    elif 'cc_mag' in details:
+        return 'mag'
+
+    raise ValueError('No valid payment type identified.')
+
+def purchase(amount, **kwargs):
     """Makes a purchase request
+    Args:
+        amount (dec): The amount being purchased.
+        **kwargs: Payment, billing, shipping, and other details.
+
+    Keyword Args:
+        cc_cardholder_name (str, optional): Name of the credit
+            cardholder.
+        cc_number (int, optional): 16 digit credit card number.
+        cc_expiry (int, optional): 4 digit (MMYY) credit card expiry.
+        cc_cvv (int, optional): 3 digit credit card CVV.
+        cc_address (str, optional): Address of the credit cardholder.
+        cc_postal_code (str, optional): Postal code/zip code of the
+            credit cardholder.
+        customer_code (str, optional): Helcim customer code.
+        card_token (str, optional): 23 digit Helcim card token.
+        cc_f4_l4 (int, optional): 8 digit "first four digits and last
+            four digits" of the credit card number
+        cc_f4_l4_skip (bool, optional): Whether to skip the F4L4
+            verification.
+        cc_mag (string, optional): Non-encrypted credit card magnetic
+            strip data.
+        serial_number (string, optional): Terminal serial number.
+        cc_mag_encrypted (str, optional): Encrypted credit card
+            magnestic strip data.
+        order_number (str, optional): An assigned order number for the
+            purchase.
+        ecommerce (bool, optional): Whether this is an e-commerce
+            transaction or not.
+        comments (str, optional): Any additional comments with this
+            transaction.
+        billing_contact_name (str, optional): Billing address contact
+            name.
+        billing_business_name (str, optional): Billing address
+            business name.
+        billing_street_1 (str, optional): Billing street address.
+        billing_street_2 (str, optional): Billing street address.
+        billing_city (str, optional): Billing city.
+        billing_province (str, optional): Billing province.
+        billing_country (str, optional): Billing country.
+        billing_postal_code (str, optional): Billing postal code.
+        billing_phone (str, optional): Billing phone number.
+        billing_fax (str, optional): Billing fax number
+        billing_email (str, optional): Billing email.
+        shipping_contact_name (str, optional): Shipping contact name.
+        shipping_business_name (str, optional): Shipping business name.
+        shipping_street_1 (str, optional): Shipping street address.
+        shipping_street_2 (str, optional): Shipping street address.
+        shipping_city (str, optional): Shipping city.
+        shipping_province (str, optional): Shipping province.
+        shipping_country (str, optional): Shipping country.
+        shipping_postal_code (str, optional): Shipping postal code.
+        shipping_phone (str, optional): Shipping phone number.
+        shipping_fax (str, optional): Shipping fax number.
+        shipping_email (str, optional): Shipping email address.
+        amount_shipping (dec, optional): Shipping cost.
+        amount_tax (dec, optional): Tax amount.
+        shipping_method (str, optional): Method of shipping.
+        tax_details (str, optional): Tax name.
     """
     # TODO: Will need to work out handling new card vs. token vs. customer code
 
-    pass
 
 def refund():
     """Makes a refund request
--- tests/test_gateway.py
+++ tests/test_gateway.py
@@ -36,3 +36,114 @@ def test_post_returns_dictionary(mock_post):
     dictionary = gateway.post('', {})
 
     assert type(dictionary) is OrderedDict
+
+def test_determine_payment_details_token():
+    details = {
+        'card_token': 'abcdefghijklmnopqrstuvwxyz',
+        'customer_code': 'CST1000',
+    }
+
+    payment_type = gateway.determine_payment_details(details)
+
+    assert payment_type == 'token'
+
+def test_determine_payment_details_customer():
+    details = {
+        'customer_code': 'CST1000',
+    }
+
+    payment_type = gateway.determine_payment_details(details)
+
+    assert payment_type == 'customer'
+
+def test_determine_payment_details_cc():
+    details = {
+        'cc_number': '1234567890123456',
+        'cc_expiry': '0125',
+    }
+
+    payment_type = gateway.determine_payment_details(details)
+
+    assert payment_type == 'cc'
+
+def test_determine_payment_details_mag_encrypted():
+    details = {
+        'cc_mag_encrypted': 'iscySW5ks7LeQQ8r4Tz7vb6el6QFpuQMbxGbh1==',
+        'serial_number': 'SERIAL1230129912',
+    }
+
+    payment_type = gateway.determine_payment_details(details)
+
+    assert payment_type == 'mage'
+
+def test_determine_payment_details_mag():
+    details = {
+        'cc_mag': '%B45**********SENSITIVE*DATA******************01?2',
+    }
+
+    payment_type = gateway.determine_payment_details(details)
+
+    assert payment_type == 'mag'
+
+def test_determine_payment_details_value_error():
+    details = {}
+
+    try:
+        gateway.determine_payment_details(details)
+    except ValueError:
+        assert True
+    else:
+        assert False
+
+def determine_payment_details_token_priority():
+    details = {
+        'card_token': 'abcdefghijklmnopqrstuvwxyz',
+        'customer_code': 'CST1000',
+        'cc_number': '1234567890123456',
+        'cc_expiry': '0125',
+        'cc_mag_encrypted': 'iscySW5ks7LeQQ8r4Tz7vb6el6QFpuQMbxGbh1==',
+        'serial_number': 'SERIAL1230129912',
+        'cc_mag': '%B45**********SENSITIVE*DATA******************01?2',
+    }
+
+    payment_type = gateway.determine_payment_details(details)
+
+    assert payment_type == 'token'
+
+def determine_payment_details_customer_priority():
+    details = {
+        'customer_code': 'CST1000',
+        'cc_number': '1234567890123456',
+        'cc_expiry': '0125',
+        'cc_mag_encrypted': 'iscySW5ks7LeQQ8r4Tz7vb6el6QFpuQMbxGbh1==',
+        'serial_number': 'SERIAL1230129912',
+        'cc_mag': '%B45**********SENSITIVE*DATA******************01?2',
+    }
+
+    payment_type = gateway.determine_payment_details(details)
+
+    assert payment_type == 'customer'
+
+def determine_payment_details_cc_priority():
+    details = {
+        'cc_number': '1234567890123456',
+        'cc_expiry': '0125',
+        'cc_mag_encrypted': 'iscySW5ks7LeQQ8r4Tz7vb6el6QFpuQMbxGbh1==',
+        'serial_number': 'SERIAL1230129912',
+        'cc_mag': '%B45**********SENSITIVE*DATA******************01?2',
+    }
+
+    payment_type = gateway.determine_payment_details(details)
+
+    assert payment_type == 'cc'
+
+def determine_payment_details_mag_encrypted_priority():
+    details = {
+        'cc_mag_encrypted': 'iscySW5ks7LeQQ8r4Tz7vb6el6QFpuQMbxGbh1==',
+        'serial_number': 'SERIAL1230129912',
+        'cc_mag': '%B45**********SENSITIVE*DATA******************01?2',
+    }
+
+    payment_type = gateway.determine_payment_details(details)
+
+    assert payment_type == 'mage'
