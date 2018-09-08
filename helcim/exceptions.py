a7516c6fd1b6fcc27e6c87f23a71bb3690a67c9d
studybuffalo <studybuffalo@gmail.com>
2018-09-08 07:36:48 -0600

    Adding docstrings to exceptions

--- helcim/exceptions.py
+++ helcim/exceptions.py
@@ -1,8 +1,12 @@
+"""Custom exceptions for package, Helcim API, and django-oscar"""
+
 try:
     from oscar.apps.payment.exceptions import PaymentError
 except ImportError:
     class PaymentError(Exception):
+        """Error to allow proper error handling with django-oscar"""
         pass
 
 class HelcimError(PaymentError):
+    """Extended django-oscar PaymentError to improve interfacing"""
     pass
