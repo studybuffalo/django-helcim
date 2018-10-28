8d690bdbbd9b1acdcde80d38ea9291c7cc68f3b3
studybuffalo <studybuffalo@gmail.com>
2018-10-28 07:40:47 -0600

    Adding admin site details

---
+++ helcim/admin.py
@@ -0,0 +1,37 @@
+"""Admin settings for Helcim Commerce API transactions."""
+from django.contrib import admin
+
+from helcim import models
+
+@admin.register(models.HelcimTransaction)
+class HelcimTransactionAdmin(admin.ModelAdmin):
+    """Admin class for the HelcimTransaction model."""
+    list_display = [
+        'transaction_type',
+        'date_response',
+        'transaction_status',
+        'amount',
+        'customer_code',
+    ]
+
+    readonly_fields = [
+        'raw_request',
+        'raw_response',
+        'transaction_status',
+        'notice_message',
+        'date_response',
+        'date_created',
+        'transaction_type',
+        'amount',
+        'currency',
+        'card_name',
+        'card_number',
+        'card_expiry',
+        'card_type',
+        'card_token',
+        'avs_response',
+        'cvv_response',
+        'approval_code',
+        'order_number',
+        'customer_code',
+    ]
