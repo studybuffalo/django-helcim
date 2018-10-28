486ce8be02b52e948c442cb89de5f26b0678e2cc
studybuffalo <studybuffalo@gmail.com>
2018-10-28 16:04:30 -0600

    Cleaning up formatting and comments for the sandbox views

--- sandbox/applications/checkout/views.py
+++ sandbox/applications/checkout/views.py
@@ -10,59 +10,69 @@ from helcim import bridge_oscar
 class PaymentDetailsView(views.PaymentDetailsView):
     """Collects and processes payment details."""
 
-    # An example view that shows how to integrate BOTH Paypal Express
-    # (see get_context_data method)and Payppal Flow (the other methods).
-    # Naturally, you will only want to use one of the two.
-
     def get_context_data(self, **kwargs):
-        """
-        Add data for Paypal Express flow.
-        """
-        # Override method so the bankcard and billing address forms can be
-        # added to the context.
-        ctx = super(PaymentDetailsView, self).get_context_data(**kwargs)
-        ctx['bankcard_form'] = kwargs.get(
-            'bankcard_form', forms.BankcardForm())
-        ctx['billing_address_form'] = kwargs.get(
-            'billing_address_form', forms.BillingAddressForm())
-        return ctx
+        """Add additional data for payment processing."""
+        context = super(PaymentDetailsView, self).get_context_data(**kwargs)
+
+        # Add the credit card data
+        context['bankcard_form'] = kwargs.get(
+            'bankcard_form', forms.BankcardForm()
+        )
+
+        # Add the billing address data
+        context['billing_address_form'] = kwargs.get(
+            'billing_address_form', forms.BillingAddressForm()
+        )
+
+        return context
 
     def post(self, request, *args, **kwargs):
-        # Override so we can validate the bankcard/billingaddress submission.
-        # If it is valid, we render the preview screen with the forms hidden
-        # within it.  When the preview is submitted, we pick up the 'action'
-        # parameters and actually place the order.
+        """Makes the POST request that triggers payment processing."""
+
+        # If action is place order, start payment processing
         if request.POST.get('action', '') == 'place_order':
             return self.do_place_order(request)
 
+        # Validate the credit card and billing address forms
         bankcard_form = forms.BankcardForm(request.POST)
         billing_address_form = forms.BillingAddressForm(request.POST)
-        if not all([bankcard_form.is_valid(),
-                    billing_address_form.is_valid()]):
-            # Form validation failed, render page again with errors
-            self.preview = False
-            ctx = self.get_context_data(
-                bankcard_form=bankcard_form,
-                billing_address_form=billing_address_form)
-            return self.render_to_response(ctx)
 
-        # Render preview with bankcard and billing address details hidden
+        if not all([
+                bankcard_form.is_valid(), billing_address_form.is_valid()
+        ]):
+            # Validation failed, render page again with errors
+            self.preview = False
+
+            context = self.get_context_data(
+                bankcard_form=bankcard_form,
+                billing_address_form=billing_address_form,
+            )
+
+            return self.render_to_response(context)
+
+        # Render the preview with the credit card and billing
+        # address details (hidden)
         return self.render_preview(
-            request, bankcard_form=bankcard_form,
-            billing_address_form=billing_address_form
+            request,
+            bankcard_form=bankcard_form,
+            billing_address_form=billing_address_form,
         )
 
     def do_place_order(self, request):
         """Helper method to check that hiddens forms were not modified."""
         bankcard_form = forms.BankcardForm(request.POST)
         billing_address_form = forms.BillingAddressForm(request.POST)
-        if not all([bankcard_form.is_valid(),
-                    billing_address_form.is_valid()]):
+
+        if not all([
+                bankcard_form.is_valid(),
+                billing_address_form.is_valid()
+        ]):
+            # Forms now have errors, return to payment page
             messages.error(request, "Invalid submission")
             return HttpResponseRedirect(reverse('checkout:payment-details'))
 
-        # Attempt to submit the order, passing the bankcard object so that it
-        # gets passed back to the 'handle_payment' method below.
+        # Submit the order along with the required payment details
+        # (these will be passed along for payment processing)
         submission = self.build_submission()
         submission['payment_kwargs']['bankcard'] = bankcard_form.bankcard
         submission['payment_kwargs']['billing_address'] = (
@@ -72,9 +82,7 @@ class PaymentDetailsView(views.PaymentDetailsView):
 
     def handle_payment(self, order_number, total, **kwargs):
         """Submit payment details to the Helcim Commerce API."""
-        # Using authorization here (two-stage model).  You could use sale to
-        # perform the auth and capture in one step.  The choice is dependent
-        # on your business model.
+        # Make a purchase request to the Helcim Commerce API
         bridge_oscar.purchase(
             order_number=order_number,
             amount=total.incl_tax,
@@ -84,9 +92,12 @@ class PaymentDetailsView(views.PaymentDetailsView):
 
         # Record payment source and event
         source_type, _ = models.SourceType.objects.get_or_create(
-            name='PayPal')
+            name='Helcim')
+
         source = source_type.sources.model(
             source_type=source_type,
-            amount_allocated=total.incl_tax, currency=total.currency)
+            amount_allocated=total.incl_tax,
+            currency=total.currency
+        )
         self.add_payment_source(source)
         self.add_payment_event('Authorised', total.incl_tax)
