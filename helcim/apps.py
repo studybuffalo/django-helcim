156a22e40cf27c0a621513d434357066d8239373
studybuffalo <studybuffalo@gmail.com>
2018-10-31 07:03:53 -0600

    Fixing app config name

--- helcim/apps.py
+++ helcim/apps.py
@@ -1,6 +1,8 @@
+"""Application configuration file for django-helcim."""
 from django.apps import AppConfig
+
 
 class HelcimConfig(AppConfig):
     """Configuration details for django-helcim."""
-    name = 'django_helcim'
+    name = 'helcim'
     verbose_name = 'django-helcim'
