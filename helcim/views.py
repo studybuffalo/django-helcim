c4979896a10fb5cb79b0ff6ac2315c3f30613597
studybuffalo <studybuffalo@gmail.com>
2018-11-01 06:56:59 -0600

    Fixing permission text (uses app, not model name)

--- helcim/views.py
+++ helcim/views.py
@@ -1,19 +1,24 @@
 """Views for Helcim Commerce API transactions."""
+from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin
 from django.views import generic
 
 from helcim import models
 
 
-class TransactionListView(generic.ListView):
+class TransactionListView(PermissionRequiredMixin, generic.ListView):
     """List of all transactions submitted made by django-helcim."""
     model = models.HelcimTransaction
-    template_name = 'helcim/transaction_list.html'
+    permission_required = 'helcim.helcim_transactions'
+    raise_exception = True
     context_object_name = 'transactions'
+    template_name = 'helcim/transaction_list.html'
 
-class TransactionDetailView(generic.DetailView):
+class TransactionDetailView(PermissionRequiredMixin, generic.DetailView):
     """Details of a specific transaction made by django-helcim."""
     model = models.HelcimTransaction
     slug_field = 'id'
     slug_url_kwargs = 'transaction_id'
-    template_name = 'helcim/transaction_detail.html'
+    permission_required = 'helcim.helcim_transactions'
+    raise_exception = True
     context_object_name = 'transaction'
+    template_name = 'helcim/transaction_detail.html'
