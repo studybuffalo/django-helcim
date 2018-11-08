519be1f87a96c3a40345f50b5a4df40c850c9e07
Study Buffalo <studybuffalo@gmail.com>
2018-11-07 21:47:48 -0700

    Update Requirements (#6)
    * Properly specifying package requirements; updating pipfile to reflect that
    
    * Updating changelog

--- helcim/__init__.py
+++ helcim/__init__.py
@@ -1,6 +1,6 @@
 # pylint: disable=missing-docstring, invalid-name
 # Versioning details
-VERSION = '0.1.0'
+VERSION = '0.1.1'
 
 # Django configuration details
 default_app_config = 'helcim.apps.HelcimConfig'
