2439733e5298a94bdde35ae683620bdcb460cfff
studybuffalo <studybuffalo@gmail.com>
2018-11-13 22:18:40 -0700

    Adding tests for the new URL logic

--- tests/helcim/test_urls.py
+++ tests/helcim/test_urls.py
@@ -1,7 +1,16 @@
 """Tests for the Helcim URLs."""
+import sys
+
+from importlib import reload, import_module
 import pytest
 
+from django.conf import settings
+from django.core.cache import cache
+from django.test import override_settings
 from django.urls import reverse
+
+from helcim import urls
+
 
 @pytest.mark.django_db
 def test_transaction_list_exists_at_desired_location(admin_client):
@@ -10,16 +19,49 @@ def test_transaction_list_exists_at_desired_location(admin_client):
 
     assert response.status_code == 200
 
-# @pytest.mark.django_db
-# def test_transaction_list_exists_with_namespace(admin_client):
-#     """Tests that transaction list URL name works."""
-#     response = admin_client.get(reverse('helcim:transaction_list'))
-
-#     assert response.status_code == 200
-
 @pytest.mark.django_db
 def test_transaction_list_exists_at_desired_url(admin_client):
     """Tests that transaction list URL works."""
     response = admin_client.get('/transactions/')
 
     assert response.status_code == 200
+
+@pytest.mark.django_db
+def test_tokens_list_exists_at_desired_location(admin_client):
+    """Tests that token list URL name works."""
+    response = admin_client.get(reverse('token_list'))
+
+    assert response.status_code == 200
+
+@pytest.mark.django_db
+def test_tokens_list_exists_at_desired_name(admin_client):
+    """Tests that token list URL name works."""
+    response = admin_client.get('/tokens/')
+
+    assert response.status_code == 200
+
+@override_settings(HELCIM_ENABLE_TOKEN_VAULT=False)
+@pytest.mark.django_db
+def test_token_urls_not_loaded_if_settings_are_false():
+    """Tests that token list URL name works."""
+    # Check that all URLs loaded initially
+    assert len(urls.urlpatterns) == 4
+
+    # Reload URLs and check that token URLs removed
+    reload(urls)
+    assert len(urls.urlpatterns) == 2
+
+@override_settings()
+@pytest.mark.django_db
+def test_token_urls_not_loaded_if_settings_are_missing():
+    """Tests that token list URL name works."""
+
+
+    # Check that all URLs loaded initially
+    reload(urls)
+    assert len(urls.urlpatterns) == 4
+
+    # Reload URLs and check that token URLs removed
+    del settings.HELCIM_ENABLE_TOKEN_VAULT
+    reload(urls)
+    assert len(urls.urlpatterns) == 2
