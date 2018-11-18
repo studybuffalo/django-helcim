8c5fa85c0d4d9b10e1836371a62b29f0cab0cd2c
studybuffalo <studybuffalo@gmail.com>
2018-11-18 14:18:39 -0700

    Disabling pylint errors

--- sandbox/applications/checkout/apps.py
+++ sandbox/applications/checkout/apps.py
@@ -9,4 +9,4 @@ class CheckoutApplication(app.CheckoutApplication):
     payment_details_view = views.PaymentDetailsView
 
 
-application = CheckoutApplication()
+application = CheckoutApplication() # pylint: disable=invalid-name
