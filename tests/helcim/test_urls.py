5c50dcf045d5ee672e2e331c5cbfabe75a531dc3
studybuffalo <studybuffalo@gmail.com>
2019-06-23 21:06:05 -0600

    Reording URL reloading to resolve test leak

--- tests/helcim/test_urls.py
+++ tests/helcim/test_urls.py
@@ -37,12 +37,15 @@ def test_tokens_list_exists_at_desired_name(admin_client):
     assert response.status_code == 200
 
 @pytest.mark.django_db
-@patch.dict('helcim.gateway.SETTINGS', {'enable_token_vault': False})
 def test_token_urls_not_loaded_if_settings_are_false():
     """Tests that token list URL name works."""
-    # Check that all URLs loaded initially
-    assert len(urls.urlpatterns) == 4
+    # Check correct number of URLs when vault disabled
+    with patch.dict('helcim.gateway.SETTINGS', {'enable_token_vault': False}):
+        reload(urls)
+        assert len(urls.urlpatterns) == 2
 
-    # Reload URLs and check that token URLs removed
-    reload(urls)
-    assert len(urls.urlpatterns) == 2
+    # Check correct number of URLs when vault enabled
+    # NB: Need to reload URLs in this order otherwise tests bleed into others
+    with patch.dict('helcim.gateway.SETTINGS', {'enable_token_vault': True}):
+        reload(urls)
+        assert len(urls.urlpatterns) == 4
