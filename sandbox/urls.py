9fac8cfd8a9a1a4fa40c3fa817b41a4cf1b7be9e
studybuffalo <studybuffalo@gmail.com>
2019-08-19 07:37:20 -0600

    Cleaning up imports Adding new oscar URLs

--- sandbox/urls.py
+++ sandbox/urls.py
@@ -1,15 +1,12 @@
 """URLs for the sandbox demo."""
-
+from django.apps import apps
 from django.conf import settings
 from django.conf.urls import url
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
-from django.conf.urls.static import static
 from django.urls import include
 
-from applications.apps import application
-
-admin.autodiscover()
 
 urlpatterns = [
     url(r'^i18n/', include('django.conf.urls.i18n')),
@@ -20,13 +17,14 @@ urlpatterns = [
     # django-helcim URLs
     url(r'^helcim/', include('helcim.urls')),
     # django-oscar URLs
-    url(r'', application.urls),
+    url(r'', include(apps.get_app_config('oscar').urls[0])),
 ]
 
 if settings.DEBUG:
     urlpatterns += staticfiles_urlpatterns()
     urlpatterns += static(
-        settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+        settings.MEDIA_URL, document_root=settings.MEDIA_ROOT
+    )
 
     import debug_toolbar
     urlpatterns = [
