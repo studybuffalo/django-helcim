75534b50ca05d9be77eb4031b4d881b881d82f96
studybuffalo <studybuffalo@gmail.com>
2019-08-19 07:37:08 -0600

    Updating settings for Oscar 2.0 Streamlining settings file

--- sandbox/settings.py
+++ sandbox/settings.py
@@ -1,11 +1,11 @@
 """Django settings file to get basic Django instance running."""
-# pylint: disable=unused-wildcard-import
 import environ
 
-from oscar import OSCAR_MAIN_TEMPLATE_DIR, get_core_apps
-from oscar.defaults import * # pylint: disable=wildcard-import
+from oscar.defaults import * # pylint: disable=wildcard-import, unused-wildcard-import
+
 
 # SETTINGS FILE
+# ----------------------------------------------------------------------------
 # Add all secret setting variables to a config.env file in the
 # test directory
 ROOT_DIR = environ.Path(__file__) - 1
@@ -13,58 +13,101 @@ PACKAGE_DIR = environ.Path(__file__) - 2
 ENV = environ.Env()
 ENV.read_env(env_file=ROOT_DIR.file('config.env'))
 
+
 # DEBUG SETTINGS
+# ----------------------------------------------------------------------------
 # Used for sandbox - DO NOT USE IN PRODUCTION
 DEBUG = True
 TEMPLATE_DEBUG = True
 SQL_DEBUG = True
 
+
 # BASE DJANGO SETTINGS
+# ----------------------------------------------------------------------------
 SECRET_KEY = ENV('DJANGO_SECRET_KEY', default='214dfsdf7ughfgdasd3446@FDF46#')
-
 SITE_ID = 1
-
 INTERNAL_IPS = ('127.0.0.1',)
-
 ROOT_URLCONF = 'urls'
-
 APPEND_SLASH = True
 
+
 # ADMIN SETTINGS
+# ----------------------------------------------------------------------------
 ADMINS = (
     # ('Your Name', 'your_email@domain.com'),
 )
 MANAGERS = ADMINS
 
+
 # EMAIL SETTINGS
+# ----------------------------------------------------------------------------
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
+
 # LOCALIZATION SETTINGS
+# ----------------------------------------------------------------------------
 USE_TZ = True
 TIME_ZONE = 'UTC'
 LANGUAGE_CODE = 'en-ca'
 USE_I18N = True
 USE_L10N = True
 
+
 # DJANGO APPLICATIONS
-INSTALLED_APPS = [
+# ----------------------------------------------------------------------------
+INSTALLED_APPS = (
     # Django Apps
-    'django.contrib.auth',
     'django.contrib.admin',
+    'django.contrib.auth',
     'django.contrib.contenttypes',
+    'django.contrib.flatpages',
+    'django.contrib.messages',
     'django.contrib.sessions',
     'django.contrib.sites',
-    'django.contrib.flatpages',
     'django.contrib.staticfiles',
     # External Apps
     'helcim',
     'debug_toolbar',
+    # Oscar Apps
+    'oscar',
+    'oscar.apps.analytics',
+    'oscar.apps.checkout',
+    'oscar.apps.address',
+    'oscar.apps.shipping',
+    'oscar.apps.catalogue',
+    'oscar.apps.catalogue.reviews',
+    'oscar.apps.partner',
+    'oscar.apps.basket',
+    'oscar.apps.payment',
+    'oscar.apps.offer',
+    'oscar.apps.order',
+    'oscar.apps.customer',
+    'oscar.apps.search',
+    'oscar.apps.voucher',
+    'oscar.apps.wishlists',
+    'oscar.apps.dashboard',
+    'oscar.apps.dashboard.reports',
+    'oscar.apps.dashboard.users',
+    'oscar.apps.dashboard.orders',
+    'oscar.apps.dashboard.catalogue',
+    'oscar.apps.dashboard.offers',
+    'oscar.apps.dashboard.partners',
+    'oscar.apps.dashboard.pages',
+    'oscar.apps.dashboard.ranges',
+    'oscar.apps.dashboard.reviews',
+    'oscar.apps.dashboard.vouchers',
+    'oscar.apps.dashboard.communications',
+    'oscar.apps.dashboard.shipping',
+    # Oscar Dependencies
     'widget_tweaks',
-] + get_core_apps([
-    'applications.checkout',
-])
+    'haystack',
+    'treebeard',
+    'sorl.thumbnail',
+    'django_tables2',
+)
 
 # DJANGO MIDDLEWARE
+# ----------------------------------------------------------------------------
 MIDDLEWARE = (
     'django.middleware.common.CommonMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
@@ -72,10 +115,13 @@ MIDDLEWARE = (
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'oscar.apps.basket.middleware.BasketMiddleware',
+    'django.contrib.flatpages.middleware.FlatpageFallbackMiddleware',
     'debug_toolbar.middleware.DebugToolbarMiddleware',
 )
 
+
 # DATABASE SETTINGS
+# ----------------------------------------------------------------------------
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
@@ -83,20 +129,20 @@ DATABASES = {
     }
 }
 ATOMIC_REQUESTS = True
-
 HAYSTACK_CONNECTIONS = {
     'default': {
         'ENGINE': 'haystack.backends.simple_backend.SimpleEngine',
     },
 }
 
+
 # TEMPLATE SETTINGS
+# ----------------------------------------------------------------------------
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [
             ROOT_DIR.path('templates'),
-            OSCAR_MAIN_TEMPLATE_DIR,
         ],
         'OPTIONS': {
             'loaders': [
@@ -111,9 +157,8 @@ TEMPLATES = [
                 'django.template.context_processors.media',
                 'django.template.context_processors.static',
                 'django.contrib.messages.context_processors.messages',
-
+                # Oscar Context Processors
                 'oscar.apps.search.context_processors.search_form',
-                'oscar.apps.promotions.context_processors.promotions',
                 'oscar.apps.checkout.context_processors.checkout',
                 'oscar.apps.customer.notifications.context_processors.notifications', # pylint: disable=line-too-long
                 'oscar.core.context_processors.metadata',
@@ -122,22 +167,30 @@ TEMPLATES = [
     }
 ]
 
+
 # MEDIA SETTINGS
+# ----------------------------------------------------------------------------
 MEDIA_ROOT = ROOT_DIR.path('media')
 MEDIA_URL = '/media/'
 
+
 # STATIC SETTINGS
+# ----------------------------------------------------------------------------
 STATIC_URL = '/static/'
 STATIC_ROOT = ROOT_DIR.path('static')
 
+
 # AUTHENTICATION SETTINGS
+# ----------------------------------------------------------------------------
 AUTHENTICATION_BACKENDS = (
     'oscar.apps.customer.auth_backends.EmailBackend',
     'django.contrib.auth.backends.ModelBackend',
 )
 LOGIN_REDIRECT_URL = '/accounts/'
 
+
 # LOGGING CONFIGURATION
+# ----------------------------------------------------------------------------
 LOGGING = {
     'version': 1,
     'disable_existing_loggers': False,
@@ -191,11 +244,15 @@ LOGGING = {
     }
 }
 
+
 # OSCAR SETTINGS
+# ----------------------------------------------------------------------------
 OSCAR_ALLOW_ANON_CHECKOUT = True
 OSCAR_SHOP_TAGLINE = 'Helcim'
 
+
 # DJANGO-HELCIM SETTINGS
+# ----------------------------------------------------------------------------
 HELCIM_API_TEST = ENV('HELCIM_API_TEST', default=True)
 HELCIM_API_URL = ENV('HELCIM_API_URL', default='')
 HELCIM_ACCOUNT_ID = ENV('HELCIM_ACCOUNT_ID', default='')
