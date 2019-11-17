4f78884d827ccb0db618d83557d18e492499994f
studybuffalo <studybuffalo@gmail.com>
2019-11-17 12:20:22 -0700

    Removing TODO code

--- tests/helcim/gateway/test_gateway_settings.py
+++ tests/helcim/gateway/test_gateway_settings.py
@@ -65,7 +65,6 @@ def test_determine_helcim_settings_missing_api_token():
     else:
         assert False
 
-# TODO: write tests to confirm that identifier is only helcim or django
 @override_settings()
 def test_determine_helcim_settings_defaults():
     """Tests that defaults is provided for expected settings."""
@@ -108,69 +107,3 @@ def test_determine_helcim_settings_defaults():
     assert helcim_settings['enable_token_vault'] is False
     assert helcim_settings['associate_user'] is True
     assert helcim_settings['enable_admin'] is False
-
-# TODO: Move this into the settings module
-# @override_settings()
-# def test_determine_save_token_status_not_specified():
-#     del settings.HELCIM_ENABLE_TOKEN_VAULT
-#     details = {
-#         'token': 'abcdefghijklmnopqrstuvw',
-#         'customer_code': 'CST1000',
-#         'token_f4l4': '11119999',
-#     }
-
-#     transaction = gateway.BaseCardTransaction(
-#         api_details=API_DETAILS, **details
-#     )
-#     status = transaction._determine_save_token_status(True)
-
-#     assert status is False
-
-
-# TODO: Move this into a new test for setting generations
-# @override_settings()
-# def test_set_api_details_no_account_id():
-#     del settings.HELCIM_ACCOUNT_ID
-
-#     try:
-#         gateway.BaseRequest()
-#     except django_exceptions.ImproperlyConfigured as error:
-#         assert True
-#         assert str(error) == 'You must define a HELCIM_ACCOUNT_ID setting'
-#     else:
-#         assert False
-
-# @override_settings()
-# def test_set_api_details_no_api_token():
-#     del settings.HELCIM_API_TOKEN
-
-#     try:
-#         gateway.BaseRequest()
-#     except django_exceptions.ImproperlyConfigured as error:
-#         assert True
-#         assert str(error) == 'You must define a HELCIM_API_TOKEN setting'
-#     else:
-#         assert False
-
-# @override_settings()
-# def test_set_api_details_none():
-#     del settings.HELCIM_API_URL
-#     del settings.HELCIM_ACCOUNT_ID
-#     del settings.HELCIM_API_TOKEN
-#     del settings.HELCIM_TERMINAL_ID
-
-#     try:
-#         gateway.BaseRequest()
-#     except django_exceptions.ImproperlyConfigured:
-#         assert True
-#     else:
-#         assert False
-
-# @override_settings()
-# def test_configure_test_transaction_not_set():
-#     del settings.HELCIM_API_TEST
-#     base = gateway.BaseRequest(api_details=API_DETAILS)
-
-#     base.configure_test_transaction()
-
-#     assert 'test' not in base.cleaned
