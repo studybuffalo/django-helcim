f279e3e007106c6a110391a8fc42b6ea59d4502a
Study Buffalo <joshua@torrance.io>
2019-11-30 16:56:34 -0700

    Update python django support 2019 11 30 (#82)
    * Updating env lists to match currently supported versions
    
    * Updating Python and Django versions
    
    * Adding python 3.8 support
    
    * Switching to list envs by python version rather than django
    
    * Adding python 3.8 environment

--- setup.py
+++ setup.py
@@ -48,16 +48,16 @@ setup(
         'Development Status :: 4 - Beta',
         'Environment :: Web Environment',
         'Framework :: Django :: 1.11',
-        'Framework :: Django :: 2.0',
         'Framework :: Django :: 2.1',
+        'Framework :: Django :: 2.2',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
         'Operating System :: Unix',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
         'Topic :: Other/Nonlisted Topic'
     ],
 )
