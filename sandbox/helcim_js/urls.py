272cfc274c486e7d2efe2ce82481ec7646bb59e5
studybuffalo <studybuffalo@gmail.com>
2020-02-01 17:17:25 -0700

    Creating new sandbox to text Helcim.js

---
+++ sandbox/helcim_js/urls.py
@@ -0,0 +1,27 @@
+"""URLs for the sandbox demo."""
+from django.conf import settings
+from django.conf.urls import url
+from django.conf.urls.static import static
+from django.contrib import admin
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from django.urls import include
+
+
+urlpatterns = [
+    # Admin URLs
+    url(r'^admin/', admin.site.urls),
+    # django-helcim URLs
+    url(r'^helcim/', include('helcim.urls')),
+]
+
+# Additional URLs if site is run in debug mode
+if settings.DEBUG:
+    urlpatterns += staticfiles_urlpatterns()
+    urlpatterns += static(
+        settings.MEDIA_URL, document_root=settings.MEDIA_ROOT
+    )
+
+    import debug_toolbar
+    urlpatterns = [
+        url(r'^__debug__/', include(debug_toolbar.urls)),
+    ] + urlpatterns
