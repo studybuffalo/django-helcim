5190261496f88bec0459977f2806eacf9e0d2017
studybuffalo <studybuffalo@gmail.com>
2020-04-21 19:07:13 -0600

    Fixing nameing of HelcimJSPayementView Removing old comment

--- sandbox/scratch/example_app/urls.py
+++ sandbox/scratch/example_app/urls.py
@@ -2,7 +2,7 @@
 from django.conf.urls import url
 from django.views.generic import TemplateView
 
-from .views import PaymentView, HelcimjsPaymentView, SuccessView
+from .views import PaymentView, HelcimJSPaymentView, SuccessView
 
 
 app_name = 'example'
@@ -11,7 +11,7 @@ urlpatterns = [
     url(r'payment/basic/$', PaymentView.as_view(), name='payment_basic'),
     url(
         r'payment/helcim-js/$',
-        HelcimjsPaymentView.as_view(),
+        HelcimJSPaymentView.as_view(),
         name='payment_helcim_js',
     ),
     url(r'complete/$', SuccessView.as_view(), name='complete'),
